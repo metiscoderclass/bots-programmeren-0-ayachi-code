fded6230cc114f258e7c787be8f443d4577b3995
Bilal ayachi <bilal.ayachi@hetmml.nl>
2017-12-12 19:59:42 +0100

    /docenten informatica laat alle docenten van het vak informatica zien : )

--- bot.js
+++ bot.js
@@ -1,13 +1,30 @@
 const TeleBot = require('telebot');
 
 const bot = new TeleBot({
-    token: '474803331:AAGrjNROmheFMnp9SzvaCpmRHaeXXG6xErQ'
+    token: '456998554:AAF7MBe8K0urL08vefgnZWiXAGWEl1CtiTA'
 });
 
 
 bot.on('/start',(msg) => {
-  let txt = "Welkom " + msg.from.first_name + " bij deze opendag bot";
+  var txt = "Welkom " + msg.from.first_name + " bij de opendag bot";
+  return bot.sendMessage(msg.from.id,txt);
 });
+
+
+
+bot.on(/^\/docenten (.+)/,(msg,props) => {
+  const vak = props.match[1];
+  if (vak == "informatica") {
+    return bot.sendMessage(msg.from.id,"Hakan Akkas, Adem Ozturk, Jurjen Hickmann")
+
+  }
+
+
+
+
+
+});
+
 
 
 
